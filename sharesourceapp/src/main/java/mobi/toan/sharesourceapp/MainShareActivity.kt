7dd21bed90fbc7d8b79a76875f8a045f52ec29ac
toantran <me@toan.mobi>
2022-11-15 16:43:35 +0700

    Finished implementing sample apps: shared pdf file from SharesourceApp and receive-write pdf file into local storage on ShareTargetApp

---
+++ sharesourceapp/src/main/java/mobi/toan/sharesourceapp/MainShareActivity.kt
@@ -0,0 +1,81 @@
+package mobi.toan.sharesourceapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.google.android.material.snackbar.Snackbar
+import mobi.toan.sharesourceapp.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+
+
+class MainShareActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.shareButton.setOnClickListener {
+            shareFile()
+        }
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    private fun shareFile() {
+        val pdfFile = getPdfFile()
+        with(pdfFile) {
+            val intent = createShareIntent(this)
+            startActivity(Intent.createChooser(intent, "Share pdf using"))
+            // TODO share intent
+        }
+    }
+
+    private fun createShareIntent(file: File): Intent {
+        Log.e("TOAN", "original: ${file.length() ?: 0}")
+        val sharedFileUri = FileProvider.getUriForFile(
+            this,
+            BuildConfig.APPLICATION_ID + ".provider",
+            file
+        )
+        checkUri(sharedFileUri)
+        return Intent().apply {
+            action = Intent.ACTION_SEND
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            setDataAndType(sharedFileUri, "application/pdf")
+        }
+    }
+
+    private fun checkUri(uri: Uri) {
+        Log.e("TOAN", "uri fileProvider: $uri")
+        val file: File? = uri.path?.let { File(it) }
+        Log.e("TOAN", "file size: ${file?.length() ?: 0}")
+    }
+
+    private fun getPdfFile(): File {
+        val rawStream = resources.openRawResource(
+            resources.getIdentifier(
+                "sample",
+                "raw", packageName
+            )
+        )
+        // writer to internal file (in real production app - this is the process of downloading
+        // pdf file to printer from network)
+        val outputFile = File.createTempFile("sample", ".pdf", cacheDir)
+        val fileWritOutputStream = FileOutputStream(outputFile)
+        fileWritOutputStream.write(rawStream.readBytes())
+        fileWritOutputStream.close()
+        rawStream.close()
+        return outputFile
+    }
+}
--- sharetargetapp/src/main/java/mobi/toan/sharetargetapp/ShareTargetActivity.kt
+++ sharetargetapp/src/main/java/mobi/toan/sharetargetapp/ShareTargetActivity.kt
@@ -1,11 +1,55 @@
 package mobi.toan.sharetargetapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.provider.OpenableColumns
+import android.util.Log
+import java.io.File
 
 class ShareTargetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share_target)
+
+        when (intent?.action) {
+            Intent.ACTION_SEND -> {
+                if (intent.type?.startsWith("application/pdf") == true) {
+                    handlePdf(intent)
+                }
+            }
+            else -> {
+                // Handle other intents, such as being started from the home screen
+            }
+        }
+    }
+
+    private fun handlePdf(returnIntent: Intent) {
+        returnIntent.data?.let { returnUri ->
+            val outputFile = File.createTempFile("receivedPdf", ".pdf", cacheDir)
+            val fileOutputStream = outputFile.outputStream()
+            with(contentResolver.openInputStream(returnUri)) {
+                this?.copyTo(fileOutputStream)
+            }
+
+            // TODO if printer needs fileOutputStream then you can use the stream here
+            // I have nothing todo here so I close it
+
+            fileOutputStream.close()
+
+            Log.e("TOAN2", "file received: ${outputFile.length()}")
+            // TODO Start the printing action here?
+            contentResolver.query(returnUri, null, null, null, null)
+        }?.use { cursor ->
+            val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+            cursor.moveToFirst()
+            Log.e("TOAN2", "name: ${cursor.getString(nameIndex)}")
+            Log.e("TOAN2", "size: ${cursor.getString(sizeIndex)}")
+
+            // TODO do this on a background thread to write file to temp folder for further processing
+        }
     }
 }
